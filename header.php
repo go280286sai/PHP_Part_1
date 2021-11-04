f59b62d0e267e15cf4c0f06765e46744c64ed1c3
go280286sai <go290286sai@gmail.com>
2021-11-04 20:17:33 +0200

    add index, nav, style
    Signed-off-by: go280286sai <go290286sai@gmail.com>

---
+++ header.php
@@ -0,0 +1,3 @@
+<?php
+echo "<header><nav><a href='index.php'>Главная</a>     <a href='task_1.php'>Урок1</a>     <a href='task_2.php'>Урок2</a>     <a href='task_3.php'>Урок3</a></nav></header>";
+?>
