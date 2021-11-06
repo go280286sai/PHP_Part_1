471039b8faa9dee205d1952e32cce068c8b38d89
go280286sai <go290286sai@gmail.com>
2021-11-06 10:12:43 +0200

    task 1
    Signed-off-by: go280286sai <go290286sai@gmail.com>

--- task_1.php
+++ task_1.php
@@ -14,7 +14,109 @@
     <?php
     include "header.php";
     ?>
-    <main></main>
+    <main>
+        <selection>
+
+        </selection>
+        <selection>
+            <table border="2px">
+                <tr>
+                    <td> <?php
+                            for ($i = 1; $i <= 10; $i++) {
+                                for ($j = 1; $j <= 1; $j++) {
+
+                                    echo "{$i} x {$j} = " . ($i * $j);
+                                    echo "<br>";
+                                }
+                            }
+                            ?></td>
+                    <td><?php
+                        for ($i = 1; $i <= 10; $i++) {
+                            for ($j = 2; $j <= 2; $j++) {
+
+                                echo "{$i} x {$j} = " . ($i * $j);
+                                echo "<br>";
+                            }
+                        }
+                        ?></td>
+                    <td><?php
+                        for ($i = 1; $i <= 10; $i++) {
+                            for ($j = 3; $j <= 3; $j++) {
+
+                                echo "{$i} x {$j} = " . ($i * $j);
+                                echo "<br>";
+                            }
+                        }
+                        ?></td>
+                    <td><?php
+                        for ($i = 1; $i <= 10; $i++) {
+                            for ($j = 4; $j <= 4; $j++) {
+
+                                echo "{$i} x {$j} = " . ($i * $j);
+                                echo "<br>";
+                            }
+                        }
+                        ?></td>
+                    <td><?php
+                        for ($i = 1; $i <= 10; $i++) {
+                            for ($j = 5; $j <= 5; $j++) {
+
+                                echo "{$i} x {$j} = " . ($i * $j);
+                                echo "<br>";
+                            }
+                        }
+                        ?></td>
+                </tr>
+                <tr>
+                    <td><?php
+                        for ($i = 1; $i <= 10; $i++) {
+                            for ($j = 6; $j <= 6; $j++) {
+
+                                echo "{$i} x {$j} = " . ($i * $j);
+                                echo "<br>";
+                            }
+                        }
+                        ?></td>
+                    <td><?php
+                        for ($i = 1; $i <= 10; $i++) {
+                            for ($j = 7; $j <= 7; $j++) {
+
+                                echo "{$i} x {$j} = " . ($i * $j);
+                                echo "<br>";
+                            }
+                        }
+                        ?></td>
+                    <td><?php
+                        for ($i = 1; $i <= 10; $i++) {
+                            for ($j = 8; $j <= 8; $j++) {
+
+                                echo "{$i} x {$j} = " . ($i * $j);
+                                echo "<br>";
+                            }
+                        }
+                        ?></td>
+                    <td><?php
+                        for ($i = 1; $i <= 10; $i++) {
+                            for ($j = 9; $j <= 9; $j++) {
+
+                                echo "{$i} x {$j} = " . ($i * $j);
+                                echo "<br>";
+                            }
+                        }
+                        ?></td>
+                    <td><?php
+                        for ($i = 1; $i <= 10; $i++) {
+                            for ($j = 10; $j <= 10; $j++) {
+
+                                echo "{$i} x {$j} = " . ($i * $j);
+                                echo "<br>";
+                            }
+                        }
+                        ?></td>
+                </tr>
+            </table>
+        </selection>
+    </main>
     <?php
     include "footer.php";
     ?>
