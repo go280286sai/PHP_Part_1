bf36334c11593b83635c4fe037f165b6c576abd5
go280286sai <go290286sai@gmail.com>
2021-11-06 11:08:02 +0200

    task2_color
    Signed-off-by: go280286sai <go290286sai@gmail.com>

--- task_2.php
+++ task_2.php
@@ -12,7 +12,164 @@
 <?php
 include "header.php";
 ?>
-<main></main>
+<main><selection>
+            <table border="2px">
+                <tr>
+                    <td> <?php
+                            for ($i = 1; $i <= 10; $i++) {
+                                for ($j = 1; $j <= 1; $j++) {
+
+                                    $str1 = "{$i} x {$j} = " . ($i * $j);
+                                    $str=(string)($str1);
+                                    $font=str_replace('1', "<font color='red'>1</font>", $str);
+                                    $font=str_replace('2', "<font color='green'>2</font>", $font);
+                                    $font=str_replace('3', "<font color='yellow'>3</font>", $font);
+                                    $font=str_replace('4', "<font color='blue'>4</font>", $font);
+                                    echo $font;
+                                    echo "<br>";
+                                }
+                            }
+                            ?></td>
+                    <td><?php
+                        for ($i = 1; $i <= 10; $i++) {
+                            for ($j = 2; $j <= 2; $j++) {
+
+                                $str1 = "{$i} x {$j} = " . ($i * $j);
+                                $str=(string)($str1);
+                                $font=str_replace('1', "<font color='red'>1</font>", $str);
+                                $font=str_replace('2', "<font color='green'>2</font>", $font);
+                                $font=str_replace('3', "<font color='yellow'>3</font>", $font);
+                                $font=str_replace('4', "<font color='blue'>4</font>", $font);
+                                echo $font;
+                                echo "<br>";
+                            }
+                        }
+                        ?></td>
+                    <td><?php
+                        for ($i = 1; $i <= 10; $i++) {
+                            for ($j = 3; $j <= 3; $j++) {
+
+                                $str1 = "{$i} x {$j} = " . ($i * $j);
+                                $str=(string)($str1);
+                                $font=str_replace('1', "<font color='red'>1</font>", $str);
+                                $font=str_replace('2', "<font color='green'>2</font>", $font);
+                                $font=str_replace('3', "<font color='yellow'>3</font>", $font);
+                                $font=str_replace('4', "<font color='blue'>4</font>", $font);
+                                echo $font;
+                                echo "<br>";
+                            }
+                        }
+                        ?></td>
+                    <td><?php
+                        for ($i = 1; $i <= 10; $i++) {
+                            for ($j = 4; $j <= 4; $j++) {
+
+                                $str1 = "{$i} x {$j} = " . ($i * $j);
+                                $str=(string)($str1);
+                                $font=str_replace('1', "<font color='red'>1</font>", $str);
+                                $font=str_replace('2', "<font color='green'>2</font>", $font);
+                                $font=str_replace('3', "<font color='yellow'>3</font>", $font);
+                                $font=str_replace('4', "<font color='blue'>4</font>", $font);
+                                echo $font;
+                                echo "<br>";
+                            }
+                        }
+                        ?></td>
+                    <td><?php
+                        for ($i = 1; $i <= 10; $i++) {
+                            for ($j = 5; $j <= 5; $j++) {
+
+                                $str1 = "{$i} x {$j} = " . ($i * $j);
+                                $str=(string)($str1);
+                                $font=str_replace('1', "<font color='red'>1</font>", $str);
+                                $font=str_replace('2', "<font color='green'>2</font>", $font);
+                                $font=str_replace('3', "<font color='yellow'>3</font>", $font);
+                                $font=str_replace('4', "<font color='blue'>4</font>", $font);
+                                echo $font;
+                                echo "<br>";
+                            }
+                        }
+                        ?></td>
+                </tr>
+                <tr>
+                    <td><?php
+                        for ($i = 1; $i <= 10; $i++) {
+                            for ($j = 6; $j <= 6; $j++) {
+
+                                $str1 = "{$i} x {$j} = " . ($i * $j);
+                                $str=(string)($str1);
+                                $font=str_replace('1', "<font color='red'>1</font>", $str);
+                                $font=str_replace('2', "<font color='green'>2</font>", $font);
+                                $font=str_replace('3', "<font color='yellow'>3</font>", $font);
+                                $font=str_replace('4', "<font color='blue'>4</font>", $font);
+                                echo $font;
+                                echo "<br>";
+                            }
+                        }
+                        ?></td>
+                    <td><?php
+                        for ($i = 1; $i <= 10; $i++) {
+                            for ($j = 7; $j <= 7; $j++) {
+
+                                $str1 = "{$i} x {$j} = " . ($i * $j);
+                                $str=(string)($str1);
+                                $font=str_replace('1', "<font color='red'>1</font>", $str);
+                                $font=str_replace('2', "<font color='green'>2</font>", $font);
+                                $font=str_replace('3', "<font color='yellow'>3</font>", $font);
+                                $font=str_replace('4', "<font color='blue'>4</font>", $font);
+                                echo $font;
+                                echo "<br>";
+                            }
+                        }
+                        ?></td>
+                    <td><?php
+                        for ($i = 1; $i <= 10; $i++) {
+                            for ($j = 8; $j <= 8; $j++) {
+
+                                $str1 = "{$i} x {$j} = " . ($i * $j);
+                                $str=(string)($str1);
+                                $font=str_replace('1', "<font color='red'>1</font>", $str);
+                                $font=str_replace('2', "<font color='green'>2</font>", $font);
+                                $font=str_replace('3', "<font color='yellow'>3</font>", $font);
+                                $font=str_replace('4', "<font color='blue'>4</font>", $font);
+                                echo $font;
+                                echo "<br>";
+                            }
+                        }
+                        ?></td>
+                    <td><?php
+                        for ($i = 1; $i <= 10; $i++) {
+                            for ($j = 9; $j <= 9; $j++) {
+
+                                $str1 = "{$i} x {$j} = " . ($i * $j);
+                                $str=(string)($str1);
+                                $font=str_replace('1', "<font color='red'>1</font>", $str);
+                                $font=str_replace('2', "<font color='green'>2</font>", $font);
+                                $font=str_replace('3', "<font color='yellow'>3</font>", $font);
+                                $font=str_replace('4', "<font color='blue'>4</font>", $font);
+                                echo $font;
+                                echo "<br>";
+                            }
+                        }
+                        ?></td>
+                    <td><?php
+                        for ($i = 1; $i <= 10; $i++) {
+                            for ($j = 10; $j <= 10; $j++) {
+
+                                $str1 = "{$i} x {$j} = " . ($i * $j);
+                                $str=(string)($str1);
+                                $font=str_replace('1', "<font color='red'>1</font>", $str);
+                                $font=str_replace('2', "<font color='green'>2</font>", $font);
+                                $font=str_replace('3', "<font color='yellow'>3</font>", $font);
+                                $font=str_replace('4', "<font color='blue'>4</font>", $font);
+                                echo $font;
+                                echo "<br>";
+                            }
+                        }
+                        ?></td>
+                </tr>
+            </table>
+        </selection></main>
 <?php
 include "footer.php";
 ?>
